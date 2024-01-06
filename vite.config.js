5200444f3c3dfd00b5158212801840e911e79d99
erik.rey <erik.rey@itriad.org.br>
2024-01-05 20:01:35 -0400

    feat: first commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
